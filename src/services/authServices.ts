b34ef91bc2347d6e3091f90b0d1ff26af6f35b2f
ManuelG141 <manuelgerman1105@gmail.com>
2025-03-28 17:43:02 -0500

    Adding logic to send user data to the database after registration

--- src/services/authServices.ts
+++ src/services/authServices.ts
@@ -1,16 +1,40 @@
-// import { User, newUser } from '../types/authTypes'
 import { Request, Response } from 'express'
+import { sendQuery } from './db'
 import { newUserType, loginUserType } from '../types/authTypes'
+import { hashSync } from 'bcrypt-ts'
 
 export const registerUser = async (req: Request<any>, res: Response<any>): Promise<void> => {
-  const user: newUserType = req.body // After Schema Validation, store validated user info
-  console.log(user)
-  console.log('User Registed...')
-  res.status(200).send('User registered')
+  const user: newUserType = {
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  } // After Schema Validation, store validated user info
+
+  const hashedPassword = hashSync(user.password, 10) // Generate Salt And Hash
+
+  try {
+    const query = 'INSERT INTO "users" (username, email, hashed_password) VALUES ($1, $2, $3)'
+    const values = [user.username, user.email, hashedPassword] // Values to insert into DB
+    await sendQuery(query, values)
+
+    console.log('User Registed...')
+    res.status(200).send('User registered')
+  } catch (error: any) {
+    if (error.code === '23505') {
+      res.status(409).send(error.detail) // Conflict
+    } else {
+      console.log(error)
+      res.status(500).send('Internal server error ' + (error.detail as string))
+    }
+  }
 }
 
 export const loginUser = async (req: Request<any>, res: Response<any>): Promise<void> => {
-  const user: loginUserType = req.body // After Schema Validation, store validated user info
+  const user: loginUserType = {
+    email: req.body.email,
+    password: req.body.password
+  } // After Schema Validation, store validated user info
+
   console.log(user)
   console.log('User Logged In...')
   res.status(200).send('User Logged In')
---
+++ src/services/db.ts
@@ -0,0 +1,43 @@
+import pg from 'pg'
+
+const pool = new pg.Pool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_ENDPOINT,
+  port: process.env.DB_PORT as unknown as number,
+  database: process.env.DB
+})
+
+export const sendQuery = async (query: string, values: any[]): Promise<object> => {
+  const client = await pool.connect() // Connect to DB
+  const result = await client.query(query, values)
+  client.release()
+
+  return result.rows // Return the result of the query
+}
+
+async function createTable (): Promise<void> {
+  const client = await pool.connect()
+  await client.query(`
+    CREATE TABLE IF NOT EXISTS "users" (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(20),
+        streak INT DEFAULT 1,
+        username VARCHAR(20) NOT NULL UNIQUE,
+        email VARCHAR(100) NOT NULL UNIQUE,
+        hashed_password VARCHAR(255) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+  `)
+  client.release()
+}
+
+createTable().then( // Create the table if it doesn't exist
+  () => {
+    console.log('Table "users" is ready.')
+  }
+).catch(
+  (error: Error) => {
+    console.error('Error creating table:', error)
+  }
+)
